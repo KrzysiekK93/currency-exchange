ac10838bf64cf9ebad5f2ea51ca39aa8c9e89e49
Krzysztof Kowalik <kowalik.k93@gmail.com>
2020-03-24 20:31:52 +0100

    Creating login page, adding vue bootstrap library

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
 Vue.use(VueRouter);
+
+// Install BootstrapVue
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
 
 const routes = [
   {
@@ -18,6 +24,15 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/login",
+    name: "Login",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Login.vue")
   }
 ];
 
---
+++ src/views/Login.vue
@@ -0,0 +1,53 @@
+<template>
+  <b-container class="bv-example-row">
+    <b-row class="justify-content-center">
+      <b-col cols="6">
+        <b-form @submit="onSubmit" v-if="show">
+          <b-form-group id="input-group-1" label="Login" label-for="login">
+            <b-form-input
+              id="login"
+              v-model="form.login"
+              required
+              placeholder="Enter login"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group
+            id="input-group-2"
+            label="Password"
+            label-for="password"
+          >
+            <b-form-input
+              id="password"
+              v-model="form.password"
+              required
+              placeholder="Enter password"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-button type="submit" variant="primary">Submit</b-button>
+        </b-form>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        login: "",
+        password: ""
+      },
+      show: true
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      console.log(this.form);
+    }
+  }
+};
+</script>
