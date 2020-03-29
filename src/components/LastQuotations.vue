827ca00a6dfce02a8edfa014f5e7de0f95aca3f1
KrzysiekK93 <kowalik.k93@gmail.com>
2020-03-29 16:56:00 +0200

    (feat): current quotations fetch data (#5)
    * Current quotations fetch data
    * Fix fetch data

---
+++ src/components/LastQuotations.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="quotations">
+    <LastQuotationsTable
+      v-bind:quotationsArray="quotationsArray"
+      v-bind:date="date"
+      v-bind:baseCurrency="baseCurrency"
+      v-bind:loader="loader"
+    />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import LastQuotationsTable from "@/components/LastQuotationsTable.vue";
+
+export default {
+  name: "quotations",
+  components: {
+    LastQuotationsTable
+  },
+  data() {
+    return {
+      quotationsArray: [],
+      date: "",
+      baseCurrency: "",
+      loader: true
+    };
+  },
+  mounted() {
+    axios
+      .get("https://api.exchangeratesapi.io/latest")
+      .then(response => {
+        let i = 1;
+        const quotationsArray = []
+        for (let element in response.data.rates) {
+          let item = {
+            number: i,
+            name: element,
+            value: response.data.rates[element]
+          };
+          i++;
+          quotationsArray.push(item);
+        }
+        this.quotationsArray = quotationsArray;
+        this.baseCurrency = response.data.base;
+        this.date = response.data.date;
+      })
+      .then(() => (this.loader = false));
+  }
+};
+</script>
---
+++ src/components/LastQuotationsTable.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="quotationsTable">
+    <div class="content">
+      <h1>Current quotations</h1>
+      <h3>Date: {{ date }}</h3>
+      <h3>Base currency: {{ baseCurrency }}</h3>
+    </div>
+    <div class="text-center" v-if="this.loader === true">
+        <b-spinner variant="success" label="Spinning"></b-spinner>
+    </div>
+    <b-table  v-if="this.loader === false" sticky-header striped hover :items="quotationsArray"></b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "quotationsTable",
+  props: {
+    quotationsArray: Array,
+    date: String,
+    baseCurrency: String,
+    loader: Boolean
+  },
+};
+</script>
+
+<style lang="scss">
+.content {
+  margin-bottom: 24px;
+}
+</style>
