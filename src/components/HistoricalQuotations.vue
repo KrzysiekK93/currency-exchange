91bdae76631112328ce646b44d25e086a6fce10e
Krzysztof Kowalik <kowalik.k93@gmail.com>
2020-03-29 17:35:37 +0200

    Historical quotations

---
+++ src/components/HistoricalQuotations.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="historicalQuotations">
+    <h2>Historical data</h2>
+    <div class="form-group">
+        <label for="currency">Pick currency</label>
+        <select class="form-control" id="currency">
+            <option>USD</option>
+            <option>EUR</option>
+        </select>
+    </div>
+   <div class="charsWrapper">
+     <VueApexCharts type="line" :options="chartOptions" :series="series"></VueApexCharts>
+   </div>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import VueApexCharts from 'vue-apexcharts'
+Vue.use(VueApexCharts)
+
+Vue.component('apexchart', VueApexCharts)
+
+export default {
+  name: "historicalQuotations",
+  components: {
+     VueApexCharts
+  },
+  data: function() {
+      return {
+        chartOptions: {
+          chart: {
+            id: 'vuechart-example'
+          },
+          xaxis: {
+            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+          }
+        },
+        series: [{
+          name: 'series-1',
+          data: [30, 40, 35, 50, 49, 60, 70, 91]
+        }]
+      }
+    },
+};
+</script>
+
+<style lang="scss">
+h2 {
+  margin: 48px 0;
+}
+
+.form-group {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    label {
+        margin-bottom: 24px;
+    }
+    .form-control {
+        max-width: 300px;
+        width: 100%;
+        margin-bottom: 24px;
+    }
+}
+</style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <LastQuotations />
+    <historicalQuotations />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import LastQuotations from "@/components/LastQuotations.vue";
+import HistoricalQuotations from "@/components/HistoricalQuotations.vue";
 
 export default {
   name: "Home",
   components: {
-    LastQuotations
+    LastQuotations,
+    HistoricalQuotations
   }
 };
 </script>
