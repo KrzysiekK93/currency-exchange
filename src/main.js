374fb9a629fc38bb4e1ee0ac747c54ef02c4aeee
Krzysztof Kowalik <kowalik.k93@gmail.com>
2020-03-29 11:10:33 +0200

    Authentication update

--- src/main.js
+++ src/main.js
@@ -2,21 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import configOptions from "./config";
 import * as firebase from "firebase";
 
 Vue.config.productionTip = false;
-
-const configOptions = {
-  apiKey: "AIzaSyBtbcpvBdM5K-rseBZyi4QQd6FogmvUUaY",
-  authDomain: "currency-exchange-31bf5.firebaseapp.com",
-  databaseURL: "https://currency-exchange-31bf5.firebaseio.com",
-  projectId: "currency-exchange-31bf5",
-  storageBucket: "currency-exchange-31bf5.appspot.com",
-  messagingSenderId: "101210870332",
-  appId: "1:101210870332:web:2e427fe503f5887c922939",
-  measurementId: "G-41P6KFLFLZ"
-};
 
 firebase.initializeApp(configOptions);
 
