148acd487f0edf80e7d495276b4f96236d9220be
GrayDelta <33991563+GrayDelta@users.noreply.github.com>
2020-04-19 00:29:29 +0200

    Database operational
    Database is working, we should move query to coresponding place and eliminate wornings.

--- src/main.js
+++ src/main.js
@@ -18,3 +18,29 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount("#app");
+
+
+/*
+//dodaje baze dla nowego użytkownika ale wali błędami i nie wyświetla strony 
+//trzeba to wrzucić gdzieś koło buttona do tworzenia konta, żeby tworzyło tylko kiedy tworzy się nowe konto
+name = "nowy"; // nazwa użytkownika, to powinien być mail, który wczytujemy prz rejerstracji
+// Creating datastore for new user
+const firestore = firebase.firestore();
+// get the data from 'Template'
+firestore.collection("users").doc("Template").get().then(function (doc) {
+    if (doc && doc.exists) {
+        var data = doc.data();
+        // saves the data to 'new_user'
+        firestore.collection("users").doc(name).set(data);
+    }
+});*/
+
+/*
+//nadpisuje nazwe użytkownika
+//to tez przy buttonie, ale pod tworzeniem bazy podmienia "DUPA" z bazy na mail XD
+const firestore = firebase.firestore();
+name = "nowy"; nazwa użytkownika, to powinien być mail, który wczytujemy prz rejerstracji
+// Update Username
+firestore.collection("users").doc(name).update({ Username: name});
+*/
+//niestety muszą być dwa osobne zapytania, w jednym nie chciało działać :/
