a53f06add9f0c12dc8bfb1a8f8fcb143b8950ba0
Krzysztof Kowalik <kowalik.k93@gmail.com>
2020-04-22 18:30:58 +0200

    Accout page

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <div class="card">
-          <div class="card-header">User Profile</div>
-          <div class="card-body">
-            <div class="hero-unit">
-              <h1>{{ user.data.displayName }}</h1>
-              <p>{{ user.data.email }}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+  <div>
+    <b-jumbotron
+      header="User Information"
+      lead="Basic information about your accout"
+    >
+      <h3>
+        Name: <u>{{ user.data.displayName }}</u>
+      </h3>
+      <h3>
+        Email: <u>{{ user.data.email }}</u>
+      </h3>
+    </b-jumbotron>
   </div>
 </template>
 <script>
@@ -26,3 +24,11 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+@media only screen and (max-width: 600px) {
+  .jumbotron .display-3 {
+    font-size: 40px !important;
+  }
+}
+</style>
---
+++ src/views/Account.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <UserInfo />
+        <AccountOperation />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import UserInfo from "@/components/UserInfo.vue";
+import AccountOperation from "@/components/AccountOperation.vue";
+
+export default {
+  name: "Account",
+  components: {
+    UserInfo,
+    AccountOperation
+  }
+};
+</script>
